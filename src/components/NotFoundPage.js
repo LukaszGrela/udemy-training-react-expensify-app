556b5aed036f25a8fba2077cd338b7f57b9bc1e2
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-03-18 17:05:33 +0000

    update to 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-    <div>404! - <Link to="/">Go Home</Link></div>
+    <div>404! - <Link to="/">ET Go Home</Link></div>
 );
 
 export default NotFoundPage;
